fc92953c21d4bc0b2d4d65e7d7ff2b54c0e8938d
rokuhati68 <junpei200268@icloud.com>
2025-07-13 08:57:19 +0900

    Update ball_script.cs

--- G_shoot/Assets/Scenes/ball_script.cs
+++ G_shoot/Assets/Scenes/ball_script.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ball_script : MonoBehaviour
 {
+    public float speed = 1;
+    public float Angle = 45;
+    private Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+        rb = GetComponent<Rigidbody2D>(); // Rigidbody2D�R���|�[�l���g���擾
+        float rad = Angle * Mathf.Deg2Rad; // �p�x�����W�A���ɕϊ�
+        Vector2 direction = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)); // �����x�N�g�����v�Z
+        rb.linearVelocity = direction * speed; // ���ˑ��x�E�p�x��ݒ�
         rb = GetComponent<Rigidbody2D>(); // Rigidbody2D�R���|�[�l���g���擾
         float rad = Angle * Mathf.Deg2Rad; // �p�x�����W�A���ɕϊ�
         Vector2 direction = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)); // �����x�N�g�����v�Z
@@ -21,11 +31,15 @@ public class ball_script : MonoBehaviour
     private void OnBecameInvisible()
     {
         // ��ʊO�ɍs�����Ƃ��I�u�W�F�N�g������
+        // ��ʊO�ɍs�����Ƃ��I�u�W�F�N�g������
         Destroy(gameObject);
     }
 
     /*private void Decidevector()
     {
+        float rad = Angle * Mathf.Deg2Rad; // �p�x�����W�A���ɕϊ�
+        Vector3 direction = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0); // �����x�N�g�����v�Z
+        vec = direction * speed * Time.deltaTime; // ���x�Ǝ��Ԃ��|���Ĉړ��ʂ��v�Z
         float rad = Angle * Mathf.Deg2Rad; // �p�x�����W�A���ɕϊ�
         Vector3 direction = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0); // �����x�N�g�����v�Z
         vec = direction * speed * Time.deltaTime; // ���x�Ǝ��Ԃ��|���Ĉړ��ʂ��v�Z
