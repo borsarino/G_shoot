fa634f463a035469b175b1506ae6f3e9db895d35
Mao Shimoda <maomakochara2@gmail.com>
2025-07-12 13:56:40 +0900

    ball_script修正(メーターからの値取得未実装)

--- G_shoot/Assets/Scenes/ball_script.cs
+++ G_shoot/Assets/Scenes/ball_script.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 
 public class ball_script : MonoBehaviour
 {
-    public float speed;
-    public float Angle;
-    Vector3 vec;
-    private Rigidbody myRigid;
+    public float speed = 1;
+    public float Angle = 45;
+    private Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // Rigidbody2Dコンポーネントを取得
+        float rad = Angle * Mathf.Deg2Rad; // 角度をラジアンに変換
+        Vector2 direction = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)); // 方向ベクトルを計算
+        rb.linearVelocity = direction * speed; // 発射速度・角度を設定
     }
 
     // Update is called once per frame
@@ -21,16 +23,16 @@ public class ball_script : MonoBehaviour
         
     }
 
-    private void Decidevector()
-    {
-        float rad = Angle * Mathf.Deg2Rad; // 角度をラジアンに変換
-        Vector3 direction = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0); // 方向ベクトルを計算
-        vec = direction * speed * Time.deltaTime; // 速度と時間を掛けて移動量を計算
-    }
-
     private void OnBecameInvisible()
     {
         // 画面外に行ったときオブジェクトを消去
         Destroy(gameObject);
     }
+
+    /*private void Decidevector()
+    {
+        float rad = Angle * Mathf.Deg2Rad; // 角度をラジアンに変換
+        Vector3 direction = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0); // 方向ベクトルを計算
+        vec = direction * speed * Time.deltaTime; // 速度と時間を掛けて移動量を計算
+    }*/
 }
