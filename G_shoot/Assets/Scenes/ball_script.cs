a9795aa8cb4b80170d0a110dc13750dc462c5661
Mao Shimoda <maomakochara2@gmail.com>
2025-07-12 13:26:47 +0900

    ball_script change

--- G_shoot/Assets/Scenes/ball_script.cs
+++ G_shoot/Assets/Scenes/ball_script.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ball_script : MonoBehaviour
 {
+    public float speed;
+    public float Angle;
+    Vector3 vec;
+    private Rigidbody myRigid;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +19,18 @@ public class ball_script : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void Decidevector()
+    {
+        float rad = Angle * Mathf.Deg2Rad; // 角度をラジアンに変換
+        Vector3 direction = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0); // 方向ベクトルを計算
+        vec = direction * speed * Time.deltaTime; // 速度と時間を掛けて移動量を計算
+    }
+
+    private void OnBecameInvisible()
+    {
+        // 画面外に行ったときオブジェクトを消去
+        Destroy(gameObject);
     }
 }
